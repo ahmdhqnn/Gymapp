7c939b872f062702d86e63e2c11e4c1817944f6d
ahmdhqnn <ahmadhaqinn@gmail.com>
2024-11-06 20:40:47 +0700

    front controller

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,11 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Gym;
+use App\Models\City;
 use Illuminate\Http\Request;
+use App\Services\FrontService;
 
 class FrontController extends Controller
 {
-    public function index() {
-        return 'halaman akan segera datang';
+    protected $frontService;
+
+    public function __construct(FrontService $frontService)
+    {
+        $this->frontService = $frontService;
+    }
+    public function index()
+    {
+        $data = $this->frontService->getFrontPageData();
+        dd($data);
+        // return view('front.index', $data);
+    }
+    public function pricing()
+    {
+        $data = $this->frontService->getSubscriptionData();
+        // return view('front.pricing', $data);
+        dd($data);
+    }
+    public function details(Gym $gym)
+    {
+        dd($gym);
+        // return view('front.details', compact('gym'));
+    }
+    public function city(City $city)
+    {
+        dd($city);
+        return view('front.city', compact('city'));
     }
 }
