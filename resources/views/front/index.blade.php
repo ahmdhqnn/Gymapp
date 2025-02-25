d724bf9c2fadf5a3b1cb991fb23b454ff5654e01
ahmdhqnn <ahmadhaqinn@gmail.com>
2025-02-25 17:13:05 +0700

    data city dan component nav

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -6,30 +6,7 @@
     <header class="relative flex flex-col w-full h-[1044px] overflow-hidden -mb-[140px]">
         <img src="{{ asset('assets/images/backgrounds/Header Illustration.svg') }}"
             class="absolute w-full h-full object-cover" alt="backgrounds">
-        <nav class="relative flex items-center justify-between w-full max-w-[1280px] mx-auto px-10 mt-10">
-            <a href="index.html">
-                <img src="{{ asset('assets/images/logos/Logo.svg') }}" class="flex shrink-0" alt="logo">
-            </a>
-            <ul class="flex items-center gap-6 justify-end">
-                <li>
-                    <a href="#" class="leading-19 tracking-03 text-[#141414]">Subscribe Plan</a>
-                </li>
-                <li>
-                    <a href="#" class="leading-19 tracking-03 text-[#141414]">Blog</a>
-                </li>
-                <li>
-                    <a href="#" class="leading-19 tracking-03 text-[#141414]">Testimonial</a>
-                </li>
-                <li>
-                    <a href="#" class="leading-19 tracking-03 text-[#141414]">About</a>
-                </li>
-                <li>
-                    <a href="#"
-                        class="leading-19 tracking-0.5 text-white font-semibold rounded-[22px] py-3 px-6 bg-[#606DE5]">My
-                        Subscription</a>
-                </li>
-            </ul>
-        </nav>
+        <x-nav/>
         <div id="hero-text" class="relative flex flex-col items-center mx-auto mt-[96px]">
             <div class="flex items-center w-fit rounded-[38px] p-2 pr-6 gap-3 bg-fitcamp-black">
                 <img src="{{ asset('assets/images/photos/triple-photo.png') }}" class="flex shrink-0 w-[88px] h-10"
@@ -193,7 +170,7 @@
                         </p>
                         <div class="flex items-center gap-3 w-fit">
                             <div class="w-16 h-16 rounded-full overflow-hidden">
-                                <img src="{{ asset('assets/images/photos/image-1.png') }}"
+                                <img src="{{ asset('assets/images/photos/Image-1.png') }}"
                                     class="w-full h-full object-cover" alt="photos">
                             </div>
                             <div class="flex flex-col gap-2">
@@ -214,7 +191,7 @@
                         </p>
                         <div class="flex items-center gap-3 w-fit">
                             <div class="w-16 h-16 rounded-full overflow-hidden">
-                                <img src="{{ asset('assets/images/photos/image-2.png') }}"
+                                <img src="{{ asset('assets/images/photos/Image-2.png') }}"
                                     class="w-full h-full object-cover" alt="photos">
                             </div>
                             <div class="flex flex-col gap-2">
@@ -235,7 +212,7 @@
                         </p>
                         <div class="flex items-center gap-3 w-fit">
                             <div class="w-16 h-16 rounded-full overflow-hidden">
-                                <img src="{{ asset('assets/images/photos/image-3.png') }}"
+                                <img src="{{ asset('assets/images/photos/Image-3.png') }}"
                                     class="w-full h-full object-cover" alt="photos">
                             </div>
                             <div class="flex flex-col gap-2">
